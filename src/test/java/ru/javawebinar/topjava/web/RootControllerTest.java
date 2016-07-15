cdd73790f85c6d1f706a2dff1c2a48190e6118d8
Nazar <nazjara@gmail.com>
2016-07-15 18:00:09 +0300

    Add tests for MealList and style.css

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,13 +1,13 @@
-package ru.javawebinar.topjava.web.user;
+package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
-import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.MealTestData.MEAL1;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 /**
@@ -28,6 +28,23 @@ public class RootControllerTest extends AbstractControllerTest {
                         allOf(
                                 hasProperty("id", is(START_SEQ)),
                                 hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+    @Test
+    public void testMealList() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("mealList"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/mealList.jsp"))
+                .andExpect(model().attribute("mealList",hasSize(6)))
+                .andExpect(model().attribute("mealList", hasItem(
+                        allOf(
+                                hasProperty("id",is(START_SEQ + 2)),
+                                hasProperty("description",is(MEAL1.getDescription())),
+                                hasProperty("dateTime",is(MEAL1.getDateTime())),
+                                hasProperty("calories",is(MEAL1.getCalories()))
                         )
                 )));
     }

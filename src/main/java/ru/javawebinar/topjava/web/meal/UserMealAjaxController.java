746f5b8835343745ad669fa40653d6e41d2cd109
Nazar <nazjara@gmail.com>
2016-07-24 17:39:14 +0300

    Add datatables, Bootstrap and Ajax to mealList

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealAjaxController.java
@@ -0,0 +1,49 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.to.UserMealWithExceed;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+/**
+ * Created by Nazar on 24.07.2016.
+ */
+@RestController
+@RequestMapping("/ajax/profile/meals")
+public class UserMealAjaxController extends AbstractUserMealController
+{
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserMealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserMeal get(@PathVariable("id") int id) {
+        return super.get(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void create(@RequestParam("description") String description,
+                       @RequestParam("calories") int calories) {
+
+        UserMeal meal = new UserMeal(LocalDateTime.now(), description, calories);
+            super.create(meal);
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.GET)
+    public List<UserMealWithExceed> getBetween(
+            @RequestParam(value = "startDate", required = false) LocalDate startDate, @RequestParam(value = "startTime", required = false) LocalTime startTime,
+            @RequestParam(value = "endDate", required = false) LocalDate endDate, @RequestParam(value = "endTime", required = false) LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
+}
